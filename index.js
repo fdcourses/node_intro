dc0f5b8ff74ca1e6ca189037c4575074411bb614
Kirill <freshjs.ftd@gmail.com>
2021-09-14 15:33:48 +0300

    Add task with solution

--- index.js
+++ index.js
@@ -1,30 +1,14 @@
 'use strict';
 
-const MyMath = require('./src/MyMath');
-const exported = require('./src/Component.js');
-const lodash = require('lodash');
+// const fsPromises1 = require('fs/promises');
 
-console.log(exported);
-/*
- resolve -> loading -> wrapping -> evaluation -> caching
+const fs = require('fs').promises;
 
- resolve - мы ищем файл
-    1. подключаем кор модули ноды (Globals)
-    2. ищем файл по имени
-      2.1 ищем файлик с расширением .js
-      2.2 ищем файлик с расширением .json
-    3. ищем папку с таким названием
-      3.1 ищем package.json -> "main"
-      3.2 index.js | index.json
-    4. ищем папку node_modules
-    5. кидем ошибку
- loading - мы читаем содержимое найденного файла
+fs.readFile('./text.txt', { encoding: 'utf-8' }).then((textData) => {
+  console.log(textData);
+  const newFileText = `\nNEW CONTENT : LOREM IPSUM BLA BLA BLA`;
+  fs.appendFile('./newFile.txt', newFileText);
+});
 
- wrapping - оборачиваем файл в функцию, докидываем инжектированные переменные
-
- evaluation - запускаем весь код в файле 1 раз
-
- caching - кешируем результат работы прошлого шага
-
-*/
-
+// создать новый файл и в него завписать сначало содержимое старого файла
+// а потом чтото свое
